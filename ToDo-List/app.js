b6d4a7cff0f31f3b8dc95398e9a2b4b0d8da6278
Yadnika Dabhade <yadnika178@gmail.com>
2022-12-15 19:42:23 +0530

    readme.md file added

--- ToDo-List/app.js
+++ ToDo-List/app.js
@@ -1,37 +1,37 @@
-const express = require("express");
+ const express = require("express");   //  it provide the express in js
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const date = require(__dirname + "/date.js");  // it provide the date
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));  //it uses the bodyParser 
+app.use(express.static("public"));  //it uses exoress.static
 
-let tasks = [];
-let works = [];
+let tasks = [];   // it sets the task
+let works = [];   //it sets the work
 
 app.get("/", function (req, res) {
-    let currentDay = date.getDate();
+    let currentDay = date.getDate();  //it provide the currentday
     res.render("index", { title: currentDay, task: tasks });
 });
 
 app.post("/", function (req, res) {
-    let newTask = req.body.newTask;
-    let buttonPressed = req.body.button;
+    let newTask = req.body.newTask;   //it is for newtask
+    let buttonPressed = req.body.button; //it is used to press the button
     if (buttonPressed === "Work") {
         works.push(newTask);
         res.redirect("/work");
     }
     else {
-        tasks.push(newTask);
-        res.redirect("/");
+        tasks.push(newTask);  //it is used to push the task
+        res.redirect("/");  // it is used to redirect 
     }
 });
 
 
-app.get("/work", function (req, res) {
+app.get("/work", function (req, res) {     //it is used for get function
     res.render("index", { title: "Work", task: works });
 });
 
---
+++ ToDo-List/readme.md
@@ -0,0 +1,23 @@
+ + # Login Signup Page 
+ + # OBJECTIVE
+ + The main of this project is about the TODO List made for sheduling the daily routine.
+
+
+ + # What task does it do? How did you implement that?
+ +  TODO List contains the list of items whatever we have created for sheduling the daily routine.
+ + After the addition of items it creats the list in the Not Completed Section . 
+ + If after creating the list if we feel this work is not for today so we can delete it from Not Completed Section.
+ + After complition of task click on check box so it will be reflected in Completed Section.
+ + After all the task get finished we can delete all the items 
+ + I implement this by using HTML, CSS, JS 
+
+
+ + # Your learnings from the project
+ + I learn to use toggle function which is used in JS 
+    
+ + # Tech Stack Used.
+ + HTML, CSS, JS.
+
+
+ + # link of Working video of my project
+ https://user-images.githubusercontent.com/99734957/207877577-01bcd3a1-5596-42bb-8b64-c92119eadecb.mp4
